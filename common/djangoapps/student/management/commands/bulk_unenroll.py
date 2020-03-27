315d98669d492f9b4abd9c19fdaae473078165e7
uzairr <uzairr@yahoo.com>
2020-03-27 19:16:11 +0500

    Refactor bulk unenroll mgmt cmd
    Bulk unenroll command is not supporting unenrolling all the learners
    from multiple courses.This PR will enhance this mgmt cmd so that
    it can support un-enrollment of learners from multiple courses.
    
    PROD-1347

--- common/djangoapps/student/management/commands/bulk_unenroll.py
+++ common/djangoapps/student/management/commands/bulk_unenroll.py
@@ -57,36 +57,42 @@ class Command(BaseCommand):
         Un-enroll the given set of users provided in csv
         """
         reader = list(unicodecsv.DictReader(csv_file))
-        users_unenrolled = {}
-        for row in reader:
-            username = row['username']
-            course_key = row['course_id']
+        column_names = [name for name in reader[0].keys()]
+        if len(column_names) == 1 and 'course_id' in column_names:
+            for row in reader:
+                self.unenroll_all_users(course_id=row['course_id'])
+            return
+        else:
+            users_unenrolled = {}
+            for row in reader:
+                username = row['username']
+                course_key = row['course_id']
 
-            try:
-                course_id = CourseKey.from_string(row['course_id'])
-            except InvalidKeyError:
-                msg = 'Invalid course id {course_id}, skipping un-enrollement for {username}, {email}'.format(**row)
-                logger.warning(msg)
-                continue
+                try:
+                    course_id = CourseKey.from_string(row['course_id'])
+                except InvalidKeyError:
+                    msg = 'Invalid course id {course_id}, skipping un-enrollement for {username}, {email}'.format(**row)
+                    logger.warning(msg)
+                    continue
 
-            try:
-                enrollment = CourseEnrollment.objects.get(user__username=username, course_id=course_id)
-                enrollment.update_enrollment(is_active=False, skip_refund=True)
-                if username in users_unenrolled:
-                    users_unenrolled[username].append(course_key.encode())
-                else:
-                    users_unenrolled[username] = [course_key.encode()]
+                try:
+                    enrollment = CourseEnrollment.objects.get(user__username=username, course_id=course_id)
+                    enrollment.update_enrollment(is_active=False, skip_refund=True)
+                    if username in users_unenrolled:
+                        users_unenrolled[username].append(course_key.encode())
+                    else:
+                        users_unenrolled[username] = [course_key.encode()]
 
-            except ObjectDoesNotExist:
-                msg = 'Enrollment for the user {} in course {} does not exist!'.format(username, course_key)
-                logger.info(msg)
+                except ObjectDoesNotExist:
+                    msg = 'Enrollment for the user {} in course {} does not exist!'.format(username, course_key)
+                    logger.info(msg)
 
-            except Exception as err:  # pylint: disable=W0703
-                msg = 'Error un-enrolling User {} from course {}: with error: {}'.format(username, course_key, err)
-                logger.error(msg, exc_info=True)
+                except Exception as err:  # pylint: disable=W0703
+                    msg = 'Error un-enrolling User {} from course {}: with error: {}'.format(username, course_key, err)
+                    logger.error(msg, exc_info=True)
 
-        logger.info("Following users have been unenrolled successfully from the following courses: {users_unenrolled}"
-                    .format(users_unenrolled=["{}:{}".format(k, v) for k, v in users_unenrolled.items()]))
+            logger.info("Following users have been unenrolled successfully from the following courses: {users_unenrolled}"
+                        .format(users_unenrolled=["{}:{}".format(k, v) for k, v in users_unenrolled.items()]))
 
     def unenroll_all_users(self, course_id):
         """
